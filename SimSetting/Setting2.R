80ee387f5719dcd8318f7a9e51a8e860219db77f
Abby Loe <97205444+AbigailLoe@users.noreply.github.com>
2023-11-27 15:53:23 -0500

    Update Setting2.R

--- SimSetting/Setting2.R
+++ SimSetting/Setting2.R
@@ -35,6 +35,7 @@ print(paste0("censoring: ", p.censoring))
 
 predList=finalPredictions = NULL #reinitialize predictions after each run.
 
+# parameters of the simulation.
 study_length.set = 2
 space.set = round(1/12, 3)
 window.set = round(1/6, 3)
@@ -43,7 +44,7 @@ study_size.set = 500
 estimand.set = 2
 discard_excess.set = T
   
-seln<-c(paste0("X_", 1:num_covs.set), "marker")
+seln<-c(paste0("X_", 1:num_covs.set), "marker") # include time as a covariate.
   
 data_package = gen.data(study_length = study_length.set, 
                           space = space.set, 
@@ -72,16 +73,18 @@ data_package = gen.data(study_length = study_length.set,
   data$X2sinX1 = data$X_2* sin(data$X_1/data$X_6)
   data$inter = data$X_1*data$X_6
   data$complicated = data$X_2*data$X_2*data$X_4
-  
-#   dd.gee <- cbind.data.frame(y=data$pseudoEst, data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_impute")])
-#   fit.geebin <- geese(y ~ X2sinX1 + fooVar +inter + complicated+ avg_exacerb_impute, 
-#                       data=dd.gee,scale.fix=TRUE,family=gaussian, id=data$sub.id,
-#                       mean.link="logit",corstr="unstructured",jack=TRUE)
-#   # no way to predict out of the geese method:
-#   ntest=nrow(data)
-#   cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_impute")]))
-#   beta=matrix(fit.geebin$beta)
-#   predList[["magicImpute"]]<-1/(1+exp(-cov.test.gee %*% beta))
+
+
+# Oracle partial history
+  dd.gee <- cbind.data.frame(y=data$pseudoEst, data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_impute")])
+  fit.geebin <- geese(y ~ X2sinX1 + fooVar +inter + complicated+ avg_exacerb_impute, 
+                      data=dd.gee,scale.fix=TRUE,family=gaussian, id=data$sub.id,
+                      mean.link="logit",corstr="unstructured",jack=TRUE)
+  # no way to predict out of the geese method:
+  ntest=nrow(data)
+  cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_impute")]))
+  beta=matrix(fit.geebin$beta)
+  predList[["magicImpute"]]<-1/(1+exp(-cov.test.gee %*% beta))
   
   
   #Oracle no history
@@ -95,8 +98,9 @@ data_package = gen.data(study_length = study_length.set,
   cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c("X2sinX1", "fooVar", "inter", "complicated")]))
   beta=matrix(fit.geebin$beta)
   predList[["magicNoHist"]]<-1/(1+exp(-cov.test.gee %*% beta))
-  
-  
+
+
+  # Oracel full history  
   dd.gee <- cbind.data.frame(y=data$pseudoEst, data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_true")])
   fit.geebin <- geese(y ~ X2sinX1 + fooVar +inter + complicated + avg_exacerb_true, 
                       data=dd.gee,scale.fix=TRUE,family=gaussian, id=data$sub.id,
@@ -106,8 +110,6 @@ data_package = gen.data(study_length = study_length.set,
   cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c("X2sinX1", "fooVar", "inter", "complicated", "avg_exacerb_true")]))
   beta=matrix(fit.geebin$beta)
   predList[["magicHist"]]<-1/(1+exp(-cov.test.gee %*% beta))
-  
-  
   
   
   #Naive LM
@@ -122,19 +124,20 @@ data_package = gen.data(study_length = study_length.set,
   predList[["naiveNoHist"]] = 1/(1+exp(-cov.test.gee %*% beta))
   
   
-#     #Naive LM
-#   dd.naive <- cbind.data.frame(y=data$pseudoEst, data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_impute")])
-#   fit.geebin <- geese(y ~ . ,
-#                       data=dd.naive,scale.fix=TRUE,family=gaussian, id=data$sub.id,
-#                       mean.link="logit",corstr="unstructured",jack=TRUE)
-#   # no way to predict out of the geese method:
-#   ntest=nrow(data)
-#   cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_impute") ]))
-#   beta=matrix(fit.geebin$beta)
-#   predList[["naiveImpute"]] = 1/(1+exp(-cov.test.gee %*% beta))
+    #Naive LM parital history
+  dd.naive <- cbind.data.frame(y=data$pseudoEst, data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_impute")])
+  fit.geebin <- geese(y ~ . ,
+                      data=dd.naive,scale.fix=TRUE,family=gaussian, id=data$sub.id,
+                      mean.link="logit",corstr="unstructured",jack=TRUE)
+  # no way to predict out of the geese method:
+  ntest=nrow(data)
+  cov.test.gee=as.matrix(cbind(rep(1,ntest), data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_impute") ]))
+  beta=matrix(fit.geebin$beta)
+  predList[["naiveImpute"]] = 1/(1+exp(-cov.test.gee %*% beta))
   
-  
-    dd.naive <- cbind.data.frame(y=data$pseudoEst, data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_true")])
+
+# Naive full history
+  dd.naive <- cbind.data.frame(y=data$pseudoEst, data[, c(paste0("X_", 1:num_covs.set), "avg_exacerb_true")])
   fit.geebin <- geese(y ~ . ,
                       data=dd.naive,scale.fix=TRUE,family=gaussian, id=data$sub.id,
                       mean.link="logit",corstr="unstructured",jack=TRUE)
@@ -145,20 +148,21 @@ data_package = gen.data(study_length = study_length.set,
   predList[["naiveHist"]] = 1/(1+exp(-cov.test.gee %*% beta))
   
   
-#   # hrf HISTORY
-#   trainDf = as.data.frame(data[,c("pseudoEst", seln, "avg_exacerb_impute")])
-#   htree.rf=hrf(x=trainDf,time=data$marker,
-#               id=as.numeric(data$ID),yindx="pseudoEst",ntrees=500,
-#               historical=FALSE,nsamp=30, se=FALSE, control = list(nodesize = 40))
-#   # this currently does not have the correct minimal number of leaves.
-#   predList[["hrfImpute"]] = htree.rf$pred_oob
+  # hrf partial history
+  trainDf = as.data.frame(data[,c("pseudoEst", seln, "avg_exacerb_impute")])
+  htree.rf=hrf(x=trainDf,time=data$marker,
+              id=as.numeric(data$ID),yindx="pseudoEst",ntrees=500,
+              historical=FALSE,nsamp=30, se=FALSE, control = list(nodesize = 40))
+  predList[["hrfImpute"]] = htree.rf$pred_oob
+# out-of-bag sample in the hrf longitudinal random forest package are independent of the sample
+# used for growing trees, making it attractive for coding.
+# predictions for an observation are stored in the pred_oob element of the trained dataframe.
   
-  
-    trainDf = as.data.frame(data[,c("pseudoEst", seln, "avg_exacerb_true")])
+  # hrf full history
+  trainDf = as.data.frame(data[,c("pseudoEst", seln, "avg_exacerb_true")])
   htree.rf=hrf(x=trainDf,time=data$marker,
                id=as.numeric(data$ID),yindx="pseudoEst",ntrees=500,
                historical=FALSE,nsamp=30, se=FALSE, control = list(nodesize = 40))
-  # this currently does not have the correct minimal number of leaves.
   predList[["hrfHistory"]] = htree.rf$pred_oob
   
     # hrf NO HISTORY
@@ -166,28 +170,28 @@ data_package = gen.data(study_length = study_length.set,
   htree.rf=hrf(x=trainDf,time=data$marker,
                id=as.numeric(data$ID),yindx="pseudoEst",ntrees=500,
                historical=FALSE,nsamp=30, se=FALSE, control = list(nodesize = 40))
-  # this currently does not have the correct minimal number of leaves.
   predList[["hrfNoHistory"]] = htree.rf$pred_oob
   
   
   finalPredictions = as.data.frame(predList)
 
-rcorr.cens(currPred, Surv(data$calTime - data$marker, data$delta))
-
-
   print("calculating c...")
   #order is true, hrf, naive
 my.surv.obj = Surv(data$calTime - data$marker, data$delta)
-  # true:
+  
+# true:
   m1 = rcorr.cens(finalPredictions$magicHist, 
                   my.surv.obj)
 
-m2 = rcorr.cens(finalPredictions$magicNoHist, 
-                  my.surv.obj)
 # here, we are concerned with mean behavior, so while one could possibly
 # impute and average, we found little difference in simulation between imputations
 # with m= 10, and a sinlge partial history case. code should be modified here
-# to deal with the partial history case.
+# to deal with the partial history case. The code used for partial history settings
+# can be obtained from the first author upon request, but it is our belief that it is
+# not necessary or particularly illuminating.
+
+m2 = rcorr.cens(finalPredictions$magicNoHist, 
+                  my.surv.obj)
 
 m3 = rcorr.cens(finalPredictions$magicImpute, 
                   my.surv.obj); m3
